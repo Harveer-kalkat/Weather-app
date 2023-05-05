2e6598d5a3dc6645c7e26e92e0b9b24b815ca034
Harveer-kalkat <harrykalkat@gmail.com>
2023-05-05 01:34:34 -0700

    Weather on landing page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
 import { WEATHER_API_URL, WETHER_API_KEY } from './api';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Forecast from './components/forecast/forecast';
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] =  useState(null);
+
+const iniData = () => {
+  const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${24.4511}&lon=${54.3969}&appid=${WETHER_API_KEY}&units=metric`);
+
+    const currentforcastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${24.4511}&lon=${54.3969}&appid=${WETHER_API_KEY}&units=metric`);
+
+    Promise.all([currentWeatherFetch, currentforcastFetch])
+    .then(async (response) => {
+      const wetherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+      
+      setCurrentWeather({city:'Abu Dhabi, AE'  , ...wetherResponse});
+      setForecast({city:'Abu Dhabi, AE', ...forecastResponse});
+    })
+    .catch((err) => console.log(err))
+}
+
+
+  useEffect(()=> {
+    iniData();
+  },[]);
+
+  
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -20,6 +43,7 @@ function App() {
     .then(async (response) => {
       const wetherResponse = await response[0].json();
       const forecastResponse = await response[1].json();
+      
       setCurrentWeather({city:searchData.label  , ...wetherResponse});
       setForecast({city:searchData.label, ...forecastResponse});
     })
